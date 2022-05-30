32de2bf82f99de2b0bdabf6cc46a865ddba4edc6
Naegato <maxime.wiatr@gmail.com>
2022-05-30 03:04:38 +0200

    file support

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,6 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+//    #[Route('/', name: 'app_react')]
+//    public function react(): Response
+//    {
+//        return $this->render('default/index.html.twig', [
+//            'route' => [
+//                'Projects List' => 'app_project',
+//                'Project Insertion' => 'app_project_insertion',
+//                'Technos List' => 'app_techno',
+//                'Techno Insertion' => 'app_techno_insertion',
+//                'Tools List' => 'app_tool',
+//                'Tool Insertion' => 'app_tool_insertion',
+//            ],
+//        ]);
+//    }
+
     #[Route('/', name: 'app_default')]
     public function index(): Response
     {
